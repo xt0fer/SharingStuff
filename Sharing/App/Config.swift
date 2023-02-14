4df617f5d6093ff6b06c1671b4dede88c388e35c
Kristofer <kris@youngers.org>
2023-02-14 09:08:07 -0500

    added bundle/container

--- Sharing/App/Config.swift
+++ Sharing/App/Config.swift
@@ -6,5 +6,5 @@
 enum Config {
     /// iCloud container identifier.
     /// Update this if you wish to use your own iCloud container.
-    static let containerIdentifier = "iCloud.com.apple.samples.cloudkit.sharing"
+    static let containerIdentifier = "iCloud.co.tioga.TiogaFolios"
 }
